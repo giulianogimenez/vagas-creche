f8380ccc40d8102d5277f1a338f0288327439528
Giuliano Gimenez <ggimenez87@gmail.com>
2018-04-05 21:45:56 -0300

    Spring Test adicionado

--- src/main/java/br/edu/fatecsjc/creche/repository/PessoaRepository.java
+++ src/main/java/br/edu/fatecsjc/creche/repository/PessoaRepository.java
@@ -5,5 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.edu.fatecsjc.creche.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	Pessoa findOneByPssNome(String string);
 	
 }
--- src/test/java/br/edu/fatecsjc/creche/PessoaRepositoryTest.java
+++ src/test/java/br/edu/fatecsjc/creche/PessoaRepositoryTest.java
@@ -2,12 +2,14 @@ package br.edu.fatecsjc.creche;
 
 import java.time.LocalDate;
 import java.time.Month;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.fatecsjc.creche.model.Pessoa;
 import br.edu.fatecsjc.creche.repository.PessoaRepository;
@@ -16,19 +18,33 @@ import junit.framework.Assert;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PessoaServiceTest {
+public class PessoaRepositoryTest {
 	@Autowired
 	PessoaService pessoaService;
 	
+	@Autowired
+	PessoaRepository pessoaRepository;
+	
 	Pessoa  p;
 	@Before
-	public void estanciarPessoas() {
+	public void setup() {
 		p = new Pessoa();
 	}
 	
 	@Test
-	public void criarPessoa() {
+	@Transactional
+	public void adicionaPessoa() {
 		p = pessoaService.criarPessoa("Camilo", LocalDate.of(1992, Month.OCTOBER, 23));
-		Assert.assertTrue(p.getNome().equals("Camilo"));
+		p = pessoaRepository.save(p);
+		Assert.assertNotNull(p.getId());
+	}
+	
+	@Test
+	@Transactional
+	public void deletaPessoa() {
+		p = pessoaService.criarPessoa("Camilo", LocalDate.of(1992, Month.OCTOBER, 23));
+		p = pessoaRepository.save(p);
+		pessoaRepository.delete(p);
+		Assert.assertNull(pessoaRepository.findOne(p.getId()));
 	}
 }
