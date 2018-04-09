509d9a7fdc2a115168ed6ff22d9df6e251decb5c
Giuliano Gimenez <ggimenez87@gmail.com>
2018-04-09 19:55:16 -0300

    - Opções de escola adicionada ao criar a inscrição.

--- src/main/java/br/edu/fatecsjc/creche/service/InscricaoService.java
+++ src/main/java/br/edu/fatecsjc/creche/service/InscricaoService.java
@@ -2,7 +2,10 @@ package br.edu.fatecsjc.creche.service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
+import br.edu.fatecsjc.creche.model.OpcaoInstituicao;
+import br.edu.fatecsjc.creche.repository.OpcaoInstituicaoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,14 +21,20 @@ public class InscricaoService {
 	private PessoaService pessoaService;
 	@Autowired
 	private InscricaoRepository inscricaoRepository;
+	@Autowired
+    private OpcaoInstituicaoRepository opcaoInstituicaoRepository;
 	
 	@Transactional
-	public Inscricao adicionarInscricao(String nome, LocalDate dataDeNascimento) {
+	public Inscricao adicionarInscricao(String nome, LocalDate dataDeNascimento, List<OpcaoInstituicao> opcaoInstituicaoList) {
 		Pessoa pessoa = pessoaService.criarPessoa(nome, dataDeNascimento);
 		Inscricao inscricao = new Inscricao();
 		inscricao.setDataCadastro(LocalDateTime.now());
 		inscricao.setPessoa(pessoa);
 		inscricao.setSitucaoInscricao(SitucaoInscricao.LISTA_DE_ESPERA);
-		return inscricaoRepository.save(inscricao);
-	}
+		opcaoInstituicaoList.forEach(o -> {
+            o.setInscricao(inscricao);
+            opcaoInstituicaoRepository.save(o);
+        });
+        return inscricaoRepository.save(inscricao);
+    }
 }
