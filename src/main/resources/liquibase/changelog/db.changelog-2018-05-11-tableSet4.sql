1464385e1b68993b3acd3d5dc649d52a00068efb
camilobmoreira <camilo.moreira@fatec.sp.gov.br>
2018-05-11 20:45:17 -0300

    Atualizando liquibase

---
+++ src/main/resources/liquibase/changelog/db.changelog-2018-05-11-tableSet4.sql
@@ -0,0 +1,17 @@
+--liquibase formatted sql
+
+--changeset camilobmoreira:03 dbms:MySQL
+CREATE TABLE uau_usuario_autorizacao (
+  usr_id BIGINT(20) NOT NULL,
+  aut_id BIGINT(20) NOT NULL,
+  CONSTRAINT FK_USR_USUARIO
+    FOREIGN KEY (usr_id)
+    REFERENCES usr_usuario (usr_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT FK_AUT_AUTORIZACAO
+    FOREIGN KEY (aut_id)
+    REFERENCES aut_autorizacao (aut_id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+);
